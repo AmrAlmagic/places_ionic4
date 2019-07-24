880ca38fa0307912c5f721b6f9511ebe681cdbed
AmrAlmagic <amr.abdalrahman94@gmail.com>
2019-07-24 18:00:28 +0200

    works with manage state

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,25 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { IonItemSliding } from '@ionic/angular';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {IonItemSliding, LoadingController} from '@ionic/angular';
+import {Subscription} from 'rxjs';
 
-import { BookingService } from './booking.service';
-import { Booking } from './booking.model';
+import {BookingService} from './booking.service';
+import {Booking} from './booking.model';
 
 @Component({
-  selector: 'app-bookings',
-  templateUrl: './bookings.page.html',
-  styleUrls: ['./bookings.page.scss'],
+    selector: 'app-bookings',
+    templateUrl: './bookings.page.html',
+    styleUrls: ['./bookings.page.scss']
 })
-export class BookingsPage implements OnInit {
-  loadedBookings: Booking[];
+export class BookingsPage implements OnInit, OnDestroy {
+    loadedBookings: Booking[];
+    private bookingSub: Subscription;
 
-  constructor(private bookingService: BookingService) { }
+    constructor(private bookingService: BookingService, private  loadingCtrl: LoadingController) {
+    }
 
-  ngOnInit() {
-    this.loadedBookings = this.bookingService.bookings;
-  }
+    ngOnInit() {
+        this.bookingService.bookings.subscribe(bookings => {
+            this.loadedBookings = bookings;
+        });
+    }
 
-  onCancelBooking(offerId: string, slidingEl: IonItemSliding) {
-    slidingEl.close();
-    // cancel booking wiht id offerId
-  }
+    onCancelBooking(bookingId: string, slidingEl: IonItemSliding) {
+        slidingEl.close();
+        this.loadingCtrl.create({message: 'Cancelling...'}).then(loadingEl => {
+            loadingEl.present();
+
+            this.bookingService.cancelBooking(bookingId).subscribe(() => {
+                loadingEl.dismiss();
+            });
+        });
+        // cancel booking wiht id offerId
+    }
+
+    ngOnDestroy() {
+        if (this.bookingSub) {
+            this.bookingSub.unsubscribe();
+        }
+    }
 }
